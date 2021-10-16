48c111f6fd459d7e503e4a005e8c81bb21e28e1b
pablogarcia77 <pabloricotero98@gmail.com>
2021-10-15 21:38:04 -0300

    Add anime & character component

---
+++ src/views/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './partials/navBar';
+import SideBar from './partials/sideBar';
+import Footer from './partials/footer';
+
+export default function Home(){
+
+    return (
+        <>
+            <NavBar />
+            <SideBar />
+            <Footer />
+        </>
+    )
+}
---
+++ src/views/partials/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function Footer(){
+
+    return (
+        <>
+            <div>Componente Footer</div>
+        </>
+    )
+}
