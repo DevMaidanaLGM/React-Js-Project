bad0421b44c832a1cba1dc4b2200ed797981753b
devmaidanalgm <dev.maidanalgm@gmail.com>
2021-10-16 02:52:56 -0300

    I'm adding the about component with some styles

--- src/views/about.jsx
+++ src/views/about.jsx
@@ -1,168 +1,85 @@
 import React from 'react';
 import { useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/about.css';
 
-
-//Esta función verifica si el formulario está vació
-const isAnEmptyForm = ({ email, subject, message }) => {
-  let yesIsEmpty = false;
-  if (email === "" && subject === ""  && message === "") {
-    yesIsEmpty = true;
-  }else{
-    yesIsEmpty = false;
-  }
-  return yesIsEmpty;
-};
-
-
-//Esta funcion valida que no haya errores
-export function validate(input){
-            let errors = {}
-            if(!input.email){
-                errors.email = "Please enter an email..."
-            } else if(!/\S+@\S+\.\S+/.test(input.email)){
-                errors.email = "Please enter a valid email..."
-            }
-
-            if (!input.subject){
-              errors.subject= "Please enter a subject..."
-            }else if(input.subject.length<10){
-              errors.subject= "The subject must have at least 10 characters... "
-            }
-
-            if (!input.message){
-                errors.message= "Please write a message..."
-            }else if(input.message.length>256 ){
-                errors.message= "You reach the character's limit for message."
-            }
-
-
-            return errors;
-}
-
-//Esta es la función principal
 export default function About(){
 
+return(
+  <div>
+    <div className ="container">
+      <div className="row d-flex justify-content-center border border-info">
+        <div className="col-12 col-lg-7 align-self-center">
 
 
-  const [state, setState] = useState({
-        email: '',
-        subject: '',
-        message: '',
-        isAnEmptyForm: true,
-
-    })
-
-    const [fails, setFails] = useState({
-            email: '',
-            subject: '',
-            message: ''
-
-    })
-
-//Función que de momento no se usa, es el submit
-    function onSubmitForm(e){
-            e.preventDefault();
-            alert(`Email es ${state.email}`)
-    }
+      		<h1 class="display-4 text-primary text-center">Leonardo Maidana</h1>
 
 
+      		<p class="lead text-secondary m-2">
+      			Hola, soy Leonardo, mucho gusto de conocerte! <br/><br/>
 
-//Función para manejar los cambios en cualquier campo
-    function onHandleChange(e){
+      			Tengo 24 años y soy un recién graduado de la carrera
+      			de Analista Programador Universitario de una Facultad de Ingeniería
+      			 en Argentina. Me encuentro constanstemente aprendiendo
+      			nuevos lenguajes y usando nuevas tecnologías para poder ser un mejor
+      			desarrollador y buscar nuevas oportunidades.
+            <br/><br/>
+      			Mi entendimiento del
+      			inglés es bastante bueno, así que puedes comunicarte conmigo tanto en
+      			español como en inglés.
+      		</p>
 
-            setState({
-                ...state,
-                [e.target.name]: e.target.value,
-                 isAnEmptyForm: isAnEmptyForm(state)
-            })
+          <div className="align-self-center" >
+            <p className="text-justify">
+            GitHub: <a
+              href="https://www.github.com/DevMaidanaLGM">@DevMaidanaLGM
+              </a> <br/>
+              Correo: <a
+                href="dev.maidanalgm@gmail.com">Dev.MaidanaLGM@Gmail.com
+                </a> <br/>
+            </p>
+          </div>
 
-            setFails(
-                validate({
-                    ...state,
-                    [e.target.name]: e.target.value
-                    })
-                )
-
-
-    }
-
-
-
-
-
-  return(
-<>
-
-<div className="about">
-  <div className="formContainer">
-    <Form className="formCustom">
-      <h1>Contact</h1>
-      <div className="formGroup">
-        <label>Email</label>
-        <input
-          type="text"
-          placeholder="Enter your email..."
-          name="email"
-          value={state.email}
-          onChange={(e) => onHandleChange(e)}
-          ></input>
-          {fails.email ? <p style={{color: 'red'}}> {fails.email}</p> : <p></p>}
+        </div>
+        <div className="col-12 col-lg-5 d-flex justify-content-center">
+          <img className="w-75 m-3 imgBorded imgLeon"src="./img/leonprofile.jpg" alt=""/>
+        </div>
       </div>
 
-      <div className="formGroup">
-        <label>Subject</label>
-        <input
-          type="text"
-          placeholder="Subject..."
-          name="subject"
-          value={state.subject}
-          onChange={(e) => onHandleChange(e)}
-          >
-        </input>
-          {fails.subject ? <p style={{color: 'red'}}>{fails.subject}</p> : <p></p>}
+
+
+      <div className="row d-flex justify-content-around border border-info">
+
+      <div className="col-12 col-lg-5 d-flex justify-content-center">>
+        <img className="w-75 m-3 imgBorded"src="./img/leonprofile.jpg" alt=""/>
       </div>
 
-      <div className="formGroup">
-        <label>Message</label>
-        <textarea
-          placeholder="Write your message..."
-          name="message"
-          value={state.message}
-          onChange={(e) => onHandleChange(e)}
-          ></textarea>
-        {fails.message ? <p style={{color: 'red'}}>{fails.message}</p> : <p></p>}
+        <div class="col-12 col-lg-7 align-self-center">
+
+
+          <h1 class="display-4 text-primary">Leonardo Maidana</h1>
+
+
+        		<p class="lead text-secondary me-2">
+        			Hola, soy Leonardo, mucho gusto de conocerte!
+              <br/><br/>
+        			Tengo 24 años y soy un recién graduado de la carrera
+        			de Analista Programador Universitario de una Facultad de Ingeniería
+        			 en Argentina. Me encuentro constanstemente aprendiendo
+        			nuevos lenguajes y usando nuevas tecnologías para poder ser un mejor
+        			desarrollador y buscar nuevas oportunidades.
+              <br/><br/>
+        			Mi entendimiento del
+        			inglés es bastante bueno, así que puedes comunicarte conmigo tanto en
+        			español como en inglés.
+        		</p>
+
+        </div>
+
       </div>
-      <Button
-        className=
-          {
-            state.isAnEmptyForm
-            || fails.email
-            || fails.subject
-            || fails.message
-            ? "button_slide" : "button_slide slide_right"
-
-          }
-        disabled={
-          state.isAnEmptyForm
-          || fails.email
-          || fails.subject
-          || fails.message
-          ? true : false }
-
-        type="button"
-        >Send Message</Button>
-        <Button
-          className="button_slide_red slide_right_red button_cancelBorder"
-          type="button"
-          >Cancelar</Button>
-    </Form>
-</div>
 
     </div>
-</>
-  )
+  </div>
+)
 
 }
---
+++ src/views/contact.jsx
@@ -0,0 +1,168 @@
+import React from 'react';
+import { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './styles/contact.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//Esta función verifica si el formulario está vació
+const isAnEmptyForm = ({ email, subject, message }) => {
+  let yesIsEmpty = false;
+  if (email === "" && subject === ""  && message === "") {
+    yesIsEmpty = true;
+  }else{
+    yesIsEmpty = false;
+  }
+  return yesIsEmpty;
+};
+
+
+//Esta funcion valida que no haya errores
+export function validate(input){
+            let errors = {}
+            if(!input.email){
+                errors.email = "Please enter an email..."
+            } else if(!/\S+@\S+\.\S+/.test(input.email)){
+                errors.email = "Please enter a valid email..."
+            }
+
+            if (!input.subject){
+              errors.subject= "Please enter a subject..."
+            }else if(input.subject.length<10){
+              errors.subject= "The subject must have at least 10 characters... "
+            }
+
+            if (!input.message){
+                errors.message= "Please write a message..."
+            }else if(input.message.length>256 ){
+                errors.message= "You reach the character's limit for message."
+            }
+
+
+            return errors;
+}
+
+//Esta es la función principal
+export default function About(){
+
+
+
+  const [state, setState] = useState({
+        email: '',
+        subject: '',
+        message: '',
+        isAnEmptyForm: true,
+
+    })
+
+    const [fails, setFails] = useState({
+            email: '',
+            subject: '',
+            message: ''
+
+    })
+
+//Función que de momento no se usa, es el submit
+    function onSubmitForm(e){
+            e.preventDefault();
+            alert(`Email es ${state.email}`)
+    }
+
+
+
+//Función para manejar los cambios en cualquier campo
+    function onHandleChange(e){
+
+            setState({
+                ...state,
+                [e.target.name]: e.target.value,
+                 isAnEmptyForm: isAnEmptyForm(state)
+            })
+
+            setFails(
+                validate({
+                    ...state,
+                    [e.target.name]: e.target.value
+                    })
+                )
+
+
+    }
+
+
+
+
+
+  return(
+<>
+
+<div className="about">
+  <div className="formContainer">
+    <Form className="formCustom">
+      <h1>Contact</h1>
+      <div className="formGroup">
+        <label>Email</label>
+        <input
+          type="text"
+          placeholder="Enter your email..."
+          name="email"
+          value={state.email}
+          onChange={(e) => onHandleChange(e)}
+          ></input>
+          {fails.email ? <p style={{color: 'red'}}> {fails.email}</p> : <p></p>}
+      </div>
+
+      <div className="formGroup">
+        <label>Subject</label>
+        <input
+          type="text"
+          placeholder="Subject..."
+          name="subject"
+          value={state.subject}
+          onChange={(e) => onHandleChange(e)}
+          >
+        </input>
+          {fails.subject ? <p style={{color: 'red'}}>{fails.subject}</p> : <p></p>}
+      </div>
+
+      <div className="formGroup">
+        <label>Message</label>
+        <textarea
+          placeholder="Write your message..."
+          name="message"
+          value={state.message}
+          onChange={(e) => onHandleChange(e)}
+          ></textarea>
+        {fails.message ? <p style={{color: 'red'}}>{fails.message}</p> : <p></p>}
+      </div>
+      <button
+        className=
+          {
+            state.isAnEmptyForm
+            || fails.email
+            || fails.subject
+            || fails.message
+            ? "button_slide" : "button_slide slide_right"
+
+          }
+        disabled={
+          state.isAnEmptyForm
+          || fails.email
+          || fails.subject
+          || fails.message
+          ? true : false }
+
+        type="button"
+        >Send Message</button>
+        <button
+          className="button_slide_red slide_right_red button_cancelBorder"
+          type="button"
+          >Cancelar</button>
+    </Form>
+</div>
+
+    </div>
+</>
+  )
+
+}
