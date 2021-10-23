45e7f1647d8cbf2033f7d32d68d5956e75f592fe
devmaidanalgm <dev.maidanalgm@gmail.com>
2021-10-22 23:38:09 -0300

    I'm adding the landing page component - not working

---
+++ src/views/landingPage.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import {  Button, Container, Row, Col } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import About from './about';
+import Home from './home';
+
+export default function landingPage(){
+
+
+
+
+    return (
+        <Container fluid className="d-flex  justify-content-center align-items-center">
+
+          <Switch>
+
+
+
+
+
+
+
+            <Row className="">
+              <Col className="col-12 col-lg-2">
+                  <Link className="linkCustom" to="/contact">Home</Link>
+              </Col>
+              <Col className="col-12 col-lg-8">
+                <Link to="/">
+                    <img className="logo fluid"src="./img/giama_logo.png"></img>
+                </Link>
+              </Col>
+              <Col className="col-12 col-lg-2">
+                  <Link className="linkCustom" to="/about">About</Link>
+              </Col>
+            </Row>
+          </Switch>
+        </Container>
+    )
+}
